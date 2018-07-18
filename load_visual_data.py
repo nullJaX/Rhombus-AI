de5fa656b2e9076c5b148b676b91974e35f9c36d
nullJaX <195643@edu.p.lodz.pl>
2018-07-18 20:12:37 +0200

    Finished preprocessing

--- load_visual_data.py
+++ load_visual_data.py
@@ -1,7 +1,7 @@
 from time import sleep, time
 from cv2 import imshow, waitKey, destroyAllWindows, resize
 from util.grabscreen import grab_screen
-from utils import extract_gameboard, reduce_gameboard, m_gameboard, draw_search_areas
+from utils import extract_gameboard, reduce_gameboard, draw_search_areas, m_gameboard_4_debug
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -19,7 +19,7 @@ def main():
         screen = grab_screen(region=(0, 32, WINDOW_WIDTH, WINDOW_HEIGHT + 32))
         screen = extract_gameboard(screen, (WINDOW_WIDTH, WINDOW_HEIGHT))
         screen = reduce_gameboard(screen, WINDOW_HEIGHT)
-        screen = m_gameboard(screen, WINDOW_HEIGHT)
+        screen = m_gameboard_4_debug(screen, WINDOW_HEIGHT)
 
         # For alignment purposes
         screen = draw_search_areas(screen)
