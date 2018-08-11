32646d118ff3d9a319b527359208b5c2573ae160
nullJaX <195643@edu.p.lodz.pl>
2018-08-11 22:00:55 +0200

    Source prepared for Python 3.x.x

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 from cv2 import getRotationMatrix2D, Canny, line, bitwise_and, FILLED, rectangle, warpAffine
-from numpy import ones, float32, uint8, sqrt
+from numpy import ones, float32, uint8
+from numpy.ma import sqrt
 
 
 # Extract only rhombus
-def extract_gameboard(image, (window_width, window_height)):
+def extract_gameboard(image, window_width, window_height):
     m = float32([[1, 0, -(window_width - window_height) / 2], [0, 1, 0]])
     image = warpAffine(image, m, (window_height, window_height))
     edges = Canny(image, 100, 200)
@@ -14,7 +15,7 @@ def extract_gameboard(image, (window_width, window_height)):
 # Extract the main square inside rhombus
 def reduce_gameboard(image, window_height):
     m = float32([[1, 0, -window_height / 4], [0, 1, -window_height / 4]])
-    image = warpAffine(image, m, (window_height / 2, window_height / 2))
+    image = warpAffine(image, m, (int(window_height / 2), int(window_height / 2)))
     return image
 
 
@@ -28,9 +29,9 @@ def m_gameboard(image, window_height):
     rect_y2 = int(height / 2 + (height / (1.5 * sqrt(2))))
 
     mask = ones(image.shape, uint8) * 255
-    rectangle(mask, (width/4 + rect_x1/2, height/4 + rect_y1/2), (width/4 + rect_x2/2, height/4 + rect_y2/2), (0, 0, 0), FILLED)
-    mask = warpAffine(mask, getRotationMatrix2D((width / 2, height / 2), 45, 1.0), (height, width))
-    rectangle(mask, (0, 0), (window_height / 2, window_height / 2), (0, 0, 0), thickness=window_height / 24)
+    rectangle(mask, (int(width/4 + rect_x1/2), int(height/4 + rect_y1/2)), (int(width/4 + rect_x2/2), int(height/4 + rect_y2/2)), (0, 0, 0), FILLED)
+    mask = warpAffine(mask, getRotationMatrix2D((int(width / 2), int(height / 2)), 45, 1.0), (height, width))
+    rectangle(mask, (0, 0), (int(window_height / 2), int(window_height / 2)), (0, 0, 0), thickness=int(window_height / 24))
     image = bitwise_and(image, mask)
     return image
 
@@ -39,10 +40,10 @@ def m_gameboard(image, window_height):
 def m_gameboard_4_debug(image, window_height):
     height, width, _ = image.shape
     mask = ones(image.shape, uint8) * 255
-    rectangle(mask, (width / 2 - width / 8, height / 2 - height / 8),
-              (width / 2 + width / 8, height / 2 + height / 8), (0, 0, 0), FILLED)
-    mask = warpAffine(mask, getRotationMatrix2D((width / 2, height / 2), 45, 1.0), (height, width))
-    rectangle(mask, (0, 0), (window_height / 2, window_height / 2), (0, 0, 0), thickness=window_height / 24)
+    rectangle(mask, (int(width / 2) - int(width / 8), int(height / 2) - int(height / 8)),
+              (int(width / 2) + int(width / 8), int(height / 2) + int(height / 8)), (0, 0, 0), FILLED)
+    mask = warpAffine(mask, getRotationMatrix2D((int(width / 2), int(height / 2)), 45, 1.0), (height, width))
+    rectangle(mask, (0, 0), (int(window_height / 2), int(window_height / 2)), (0, 0, 0), thickness=int(window_height / 24))
     image = bitwise_and(image, mask)
     return image
 
@@ -50,10 +51,10 @@ def m_gameboard_4_debug(image, window_height):
 # Draw cross and search areas for alignment
 def draw_search_areas(image):
     height, width, _ = image.shape
-    rectangle(image, (width / 3, 0), (2 * width / 3, height / 6), (255, 0, 0))  # top
-    rectangle(image, (width / 3, 5*height/6), (2 * width / 3, height), (255, 0, 0))  # bottom
-    rectangle(image, (0, height / 3), (width / 6, 2 * height / 3), (255, 0, 0))  # left
-    rectangle(image, (5 * width / 6, height / 3), ( width , 2 * height / 3), (255, 0, 0))  # right
-    line(image, (0, height / 2), (width, height / 2), (0, 255, 0))
-    line(image, (width / 2, 0), (width / 2, height), (0, 255, 0))
+    rectangle(image, (int(width / 3), 0), (int(2 * width / 3), int(height / 6)), (255, 0, 0))  # top
+    rectangle(image, (int(width / 3), int(5*height/6)), (int(2 * width / 3), height), (255, 0, 0))  # bottom
+    rectangle(image, (0, int(height / 3)), (int(width / 6), int(2 * height / 3)), (255, 0, 0))  # left
+    rectangle(image, (int(5 * width / 6), int(height / 3)), ( width , int(2 * height / 3)), (255, 0, 0))  # right
+    line(image, (0, int(height / 2)), (width, int(height / 2)), (0, 255, 0))
+    line(image, (int(width / 2), 0), (int(width / 2), height), (0, 255, 0))
     return image
